a4eae386b950670c2fd4f545795ce84188a57045
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-03-30 22:47:19 +0700

    Latihan menampilkan teks "Hello, world" ke layar

---
+++ src/Course/SoloLearn/BasicConcept/SemicolonsInJava.java
@@ -0,0 +1,11 @@
+package Course.SoloLearn.BasicConcept;
+
+public class SemicolonsInJava {
+
+    public static void main (String[]args) {
+
+        System.out.println("Hello, World!");
+
+    }
+
+}
