192bdee8148b625c2d3eb53fe4f1b062dac70e40
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-03-31 09:45:53 +0700

    Input menggunakan java scanner

---
+++ src/Course/SoloLearn/BasicConcept/GettingUserInput.java
@@ -0,0 +1,15 @@
+package Course.SoloLearn.BasicConcept;
+
+import java.util.Scanner;
+public class GettingUserInput {
+
+        public static void main(String[ ] args) {
+
+            Scanner sc = new Scanner(System.in);
+            String st = sc.nextLine();
+
+            System.out.println(st);
+
+        }
+
+}
