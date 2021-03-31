66fc44a5b35e04b55f8295c2be632fc4c946e869
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-03-31 10:02:43 +0700

    Menggunakan operator penjumlahan (+)

---
+++ src/Course/SoloLearn/BasicConcept/Addition.java
@@ -0,0 +1,14 @@
+package Course.SoloLearn.BasicConcept;
+
+public class Addition {
+
+    public static void main (String[] args) {
+
+        int x = 2; int y = 4;
+        int result = x + y;
+
+        System.out.println(result);
+
+    }
+
+}
