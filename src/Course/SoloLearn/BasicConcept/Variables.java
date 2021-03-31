fbe382448a82832aba5ad160924c58cb1bb22bd3
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-03-31 09:34:27 +0700

    Identifikasi tipe data variabel

---
+++ src/Course/SoloLearn/BasicConcept/Variables.java
@@ -0,0 +1,15 @@
+package Course.SoloLearn.BasicConcept;
+
+public class Variables {
+    public static void main(String[ ]args) {
+
+        String name = "John";
+        int age = 24;
+        double height = 189.87;
+
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("Height : " + height);
+
+    }
+}
