04a999d0481fc01a8e9fc46ff28e8aadd16601eb
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-03-31 23:30:18 +0700

    Menggunakan operator perkalian (*)

---
+++ src/Course/SoloLearn/BasicConcept/Multiplication.java
@@ -0,0 +1,14 @@
+package Course.SoloLearn.BasicConcept;
+
+public class Multiplication {
+
+    public static void main (String[] args) {
+
+        int x = 15; int y = 4;
+        int result = x / y;
+
+        System.out.println(result);
+
+    }
+
+}
