81d4359ddebfbcf59ce95de3b9228cab4757607a
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-01 10:31:46 +0700

    Pernyataan if diikuti oleh pernyataan lain (else)opsional, yang dijalankan ketika kondisi bernilai false

---
+++ src/Course/SoloLearn/ConditionalAndLoop/IfELseStatements.java
@@ -0,0 +1,21 @@
+package Course.SoloLearn.ConditionalAndLoop;
+
+public class IfELseStatements {
+
+    public static void main(String[] args) {
+
+        int x = 10;  int y = 5;
+
+        if ( x > y ) {
+
+            System.out.println(x);
+
+        } else {
+
+            System.out.println(y);
+
+        }
+
+    }
+
+}
