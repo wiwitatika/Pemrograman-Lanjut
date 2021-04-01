e83bd459afc319075bdaac7708a40c80165f1637
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-01 09:01:09 +0700

    Menggunakan operator modulo/sisa bagi (%)

---
+++ src/Course/SoloLearn/BasicConcept/Modulo.java
@@ -0,0 +1,14 @@
+package Course.SoloLearn.BasicConcept;
+
+public class Modulo {
+
+    public static void main (String[] args) {
+
+        int x = 8, y = 5;
+        int result = x % y;
+
+        System.out.println(result);
+
+    }
+
+}
