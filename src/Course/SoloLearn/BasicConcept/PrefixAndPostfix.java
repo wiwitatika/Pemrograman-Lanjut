636c1f27ae167b565930d0ab2641df52e1a51aa4
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-01 09:29:36 +0700

    Postfix: Nilai variabel pertama kali digunakan dalam ekspresi, kemudian dinaikkan.

---
+++ src/Course/SoloLearn/BasicConcept/PrefixAndPostfix.java
@@ -0,0 +1,13 @@
+package Course.SoloLearn.BasicConcept;
+
+public class PrefixAndPostfix {
+
+    public static void main (String[] args) {
+
+        int x = 14;
+
+        System.out.println(x++);
+
+    }
+
+}
