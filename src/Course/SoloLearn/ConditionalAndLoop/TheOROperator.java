c6333f70644b06d149e9f9090040cfaa5c7dd308
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-01 10:40:40 +0700

    Operator OR (||) memeriksa apakah salah satu kondisinya benar?

---
+++ src/Course/SoloLearn/ConditionalAndLoop/TheOROperator.java
@@ -0,0 +1,22 @@
+package Course.SoloLearn.ConditionalAndLoop;
+
+public class TheOROperator {
+
+    public static void main(String[] args) {
+
+        int a = 11; int b = 12;
+        int c = 40;
+
+        if (a > 100 || b > 3) {
+
+            System.out.println(a);
+
+        } else {
+
+            System.out.println(c);
+
+        }
+
+    }
+
+}
