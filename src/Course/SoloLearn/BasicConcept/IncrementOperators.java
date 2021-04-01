1756f43e83eb91b5a7b3c0cabb0ba503fee82095
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-01 09:20:29 +0700

    Operator increment cara ringkas untuk menambah nilai variabel satu per satu. (x = x + 1; disederhanakan menjadi ++ x;)

---
+++ src/Course/SoloLearn/BasicConcept/IncrementOperators.java
@@ -0,0 +1,14 @@
+package Course.SoloLearn.BasicConcept;
+
+public class IncrementOperators {
+
+    public static  void main (String[] args) {
+
+        int a = 10;
+        ++a;
+
+        System.out.println(a);
+
+    }
+
+}
