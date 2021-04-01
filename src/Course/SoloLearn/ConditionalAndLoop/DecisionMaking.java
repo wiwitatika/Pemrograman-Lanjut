5ebe77fe72025a4911bb6fa03f4c94cbca873b94
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-01 10:27:03 +0700

    Pernyataan bersyarat IF, apakah true? (==)

---
+++ src/Course/SoloLearn/ConditionalAndLoop/DecisionMaking.java
@@ -0,0 +1,17 @@
+package Course.SoloLearn.ConditionalAndLoop;
+
+public class DecisionMaking {
+
+    public static void main(String[] args) {
+
+        int x = 5;
+
+        if ( x == 5 ) {
+
+            System.out.println("Yeah");
+
+        }
+
+    }
+
+}
