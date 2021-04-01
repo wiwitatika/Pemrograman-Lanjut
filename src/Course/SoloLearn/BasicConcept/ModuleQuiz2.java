52540eba5a49e3717faf6fa11166966fc279c1ea
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-01 10:00:33 +0700

    Quiz

--- src/Course/SoloLearn/BasicConcept/ModuleQuiz2.java
+++ src/Course/SoloLearn/BasicConcept/ModuleQuiz2.java
@@ -1,4 +1,15 @@
 package Course.SoloLearn.BasicConcept;
 
 public class ModuleQuiz2 {
+
+    public static void main(String[] args) {
+
+        String name;
+        name = "David";
+
+
+        System.out.println(name);
+
+    }
+
 }
