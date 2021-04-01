44418ee5d5ac127f23df93d56e7a4d097f0d2d20
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-01 09:44:34 +0700

    String: objek yang merepresentasikan rangkaian karakter

---
+++ src/Course/SoloLearn/BasicConcept/Strings.java
@@ -0,0 +1,15 @@
+package Course.SoloLearn.BasicConcept;
+
+public class Strings {
+
+    public static void main (String[] args) {
+
+        String var;
+        var = "Hello";
+
+
+        System.out.println(var);
+
+    }
+
+}
