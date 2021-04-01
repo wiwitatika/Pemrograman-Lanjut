3f9541eb9405eeecf4a6d6cc4a54e24c32095b52
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-01 09:39:41 +0700

    Operator assignment (=) memberikan nilai ke variabel

---
+++ src/Course/SoloLearn/BasicConcept/AssignmentOperators.java
@@ -0,0 +1,14 @@
+package Course.SoloLearn.BasicConcept;
+
+public class AssignmentOperators {
+
+    public static void main (String[] args) {
+
+        int x = 25; int y;
+        y = x - 12;
+
+        System.out.println(y);
+
+    }
+
+}
