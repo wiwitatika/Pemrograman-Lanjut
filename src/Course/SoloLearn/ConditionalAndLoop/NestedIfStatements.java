2d419ec543149d3d095b5825fee2cea66e7b5d07
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-01 10:35:27 +0700

    Menggunakan satu pernyataan if-else di dalam pernyataan if atau else lainnya

---
+++ src/Course/SoloLearn/ConditionalAndLoop/NestedIfStatements.java
@@ -0,0 +1,21 @@
+package Course.SoloLearn.ConditionalAndLoop;
+
+public class NestedIfStatements {
+
+    public static void main(String[] args) {
+
+        int x = 37;
+
+        if (x > 22) {
+
+            if (x > 31) {
+
+                System.out.println("it works!");
+
+            }
+
+        }
+
+    }
+
+}
