c1185da8b1bbfdc743c58c4b358230b9ef20b4c0
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-02 10:35:53 +0700

    Quiz

---
+++ src/Course/SoloLearn/ConditionalAndLoop/Module2Quiz4.java
@@ -0,0 +1,15 @@
+package Course.SoloLearn.ConditionalAndLoop;
+
+public class Module2Quiz4 {
+
+    public static void main(String[] args) {
+
+        for (int x = 0; x < 5; x++) {
+
+            System.out.println("in a loop");
+
+        }
+
+    }
+
+}
