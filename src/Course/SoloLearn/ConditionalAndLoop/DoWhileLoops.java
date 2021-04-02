e41b9761068546564fc1db85f0aec3dc789de27a
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-02 10:15:34 +0700

    Do while loop mirip dengan while loop, do while loop dijamin akan dieksekusi setidaknya satu kali

---
+++ src/Course/SoloLearn/ConditionalAndLoop/DoWhileLoops.java
@@ -0,0 +1,21 @@
+package Course.SoloLearn.ConditionalAndLoop;
+
+public class DoWhileLoops {
+
+    public static void main(String[] args) {
+
+        int x = 1;
+
+        do {
+
+            System.out.println(x);
+
+            x++;
+
+        }
+
+        while (x <= 5);
+
+    }
+
+}
