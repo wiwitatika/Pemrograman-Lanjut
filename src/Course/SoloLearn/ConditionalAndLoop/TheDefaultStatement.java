98df9f501cf6a0d49e5a37ecbfcfbb424a599947
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-02 09:56:25 +0700

    Default digunakan untuk melakukan tugas ketika tidak ada case yang cocok.

---
+++ src/Course/SoloLearn/ConditionalAndLoop/TheDefaultStatement.java
@@ -0,0 +1,23 @@
+package Course.SoloLearn.ConditionalAndLoop;
+
+public class TheDefaultStatement {
+
+    public static void main(String[] args) {
+
+        int a = 11; int b = 12; int c = 40;
+
+        switch (a) {
+            case 40:
+
+                System.out.println(b);
+
+                break;
+
+            default:
+
+                System.out.println(c);
+        }
+
+    }
+
+}
