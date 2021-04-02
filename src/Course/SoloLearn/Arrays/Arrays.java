e3398e2b9570b573ffec7641f03d87187ecbc807
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-02 10:58:04 +0700

    Menghitung jumlah elemen myArray menggunakan for loop

---
+++ src/Course/SoloLearn/Arrays/Arrays.java
@@ -0,0 +1,20 @@
+package Course.SoloLearn.Arrays;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+
+        int myArray[] = {1,2,3,4};
+        double sum = 0.0;
+
+        for (int x = 0; x < 4; x++) {
+
+            sum += myArray[x];
+
+        }
+
+        System.out.println(sum);
+
+    }
+
+}
