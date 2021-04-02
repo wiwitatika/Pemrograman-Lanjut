47f08076b2d84ca3847b7ec0cef76087cf60c133
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-02 22:27:21 +0700

    Memanggil indeks array [0][2] pada array multidimensi

---
+++ src/Course/SoloLearn/Arrays/MultidimensionalArrays2.java
@@ -0,0 +1,13 @@
+package Course.SoloLearn.Arrays;
+
+public class MultidimensionalArrays2 {
+
+    public static void main(String[] args) {
+
+        int array[ ][ ] =  {{3, 5, 8}, {7, 54, 1, 12, 4}};
+
+        System.out.println(array[0][2]);
+
+    }
+
+}
