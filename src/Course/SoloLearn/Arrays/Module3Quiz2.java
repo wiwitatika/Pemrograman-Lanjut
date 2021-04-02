ac1552c94fbc5a531df28113510e57e2d2121ce0
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-02 22:44:03 +0700

    Quiz

---
+++ src/Course/SoloLearn/Arrays/Module3Quiz2.java
@@ -0,0 +1,25 @@
+package Course.SoloLearn.Arrays;
+
+public class Module3Quiz2 {
+
+    public static void main(String[] args) {
+
+        int result = 0;
+
+        for (int i = 0; i < 5; i++) {
+
+            if (i == 3) {
+
+                result += 10;
+
+            } else {
+
+                result += i;
+
+            }
+        }
+        System.out.println(result);
+
+    }
+
+}
