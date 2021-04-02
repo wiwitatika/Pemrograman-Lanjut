b0d4d76de77165b09351e88fdabacdf5d845459d
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-02 11:05:00 +0700

    For each digunakan untuk melintasi elemen dalam array.

---
+++ src/Course/SoloLearn/Arrays/EnhancedForLoop.java
@@ -0,0 +1,18 @@
+package Course.SoloLearn.Arrays;
+
+public class EnhancedForLoop {
+
+    public static void main(String[] args) {
+
+        int arr[] = {2,3,4,9};
+        int total = 0;
+
+        for(int x : arr) {
+
+            total += x;
+
+        }
+
+    }
+
+}
