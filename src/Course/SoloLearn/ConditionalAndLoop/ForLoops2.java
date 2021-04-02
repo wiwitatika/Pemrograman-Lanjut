091099f8c6a739dbb2dc90042ff8a17c05f4f60e
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-02 10:10:55 +0700

    For loop menulis perulangan secara efisien yang perlu dieksekusi dalam jumlah tertentu.

---
+++ src/Course/SoloLearn/ConditionalAndLoop/ForLoops2.java
@@ -0,0 +1,15 @@
+package Course.SoloLearn.ConditionalAndLoop;
+
+public class ForLoops2 {
+
+    public static void main(String[] args) {
+
+        for (int i = 2; i < 10; i = i*i) {
+
+            System.out.println(i);
+
+        }
+
+    }
+
+}
