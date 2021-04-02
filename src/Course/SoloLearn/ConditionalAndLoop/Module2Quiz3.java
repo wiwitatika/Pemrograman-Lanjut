dc3d3846dbfebbb2b7910178e6be6be66c74e519
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-02 10:33:33 +0700

    Quiz

---
+++ src/Course/SoloLearn/ConditionalAndLoop/Module2Quiz3.java
@@ -0,0 +1,18 @@
+package Course.SoloLearn.ConditionalAndLoop;
+
+public class Module2Quiz3 {
+
+    public static void main(String[] args) {
+
+        int a = 144;
+        int b = 33;
+
+        if (a > 15 && b <= 72) {
+
+            System.out.println("You rock!");
+
+        }
+
+    }
+
+}
