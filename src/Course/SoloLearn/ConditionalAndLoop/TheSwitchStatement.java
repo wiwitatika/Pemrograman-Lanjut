dad96f971dfb3be3109e0c72687c8f45324c0205
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-02 09:42:13 +0700

    Pernyataan switch menguji kesetaraan variabel terhadap daftar nilai (case).

---
+++ src/Course/SoloLearn/ConditionalAndLoop/TheSwitchStatement.java
@@ -0,0 +1,25 @@
+package Course.SoloLearn.ConditionalAndLoop;
+
+public class TheSwitchStatement {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        switch (x) {
+            case 10:
+
+                System.out.println("A");
+
+                break;
+
+            case 20:
+
+                System.out.println("B");
+
+                break;
+        }
+
+    }
+
+}
