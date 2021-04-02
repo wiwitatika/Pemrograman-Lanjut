61c4b150e49f15c1ba6114bc4996ec07bb21a6db
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-02 11:09:12 +0700

    Memanggil indeks array [1][1] pada array multidimensi

---
+++ src/Course/SoloLearn/Arrays/MultidimensionalsArrays.java
@@ -0,0 +1,14 @@
+package Course.SoloLearn.Arrays;
+
+public class MultidimensionalsArrays {
+
+    public static void main(String[] args) {
+
+        String array[ ][ ] =  { {"Hey", "John", "Bye"},
+                                {"John", "Johnson", "Hello"} };
+
+        System.out.println(array[1][1]);
+
+    }
+
+}
