3a7fa5433509bfe6a2b43e6286f7071b3a445459
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-02 10:03:06 +0700

    While loop berulang kali mengeksekusi pernyataan target selama kondisi bernilai benar.

---
+++ src/Course/SoloLearn/ConditionalAndLoop/WhileLoops.java
@@ -0,0 +1,19 @@
+package Course.SoloLearn.ConditionalAndLoop;
+
+public class WhileLoops {
+
+    public static void main(String[] args) {
+
+        int x = 0;
+        int y = 5;
+
+        while (x < y) {
+
+            System.out.println("Hello");
+            x++;
+
+        }
+
+    }
+
+}
