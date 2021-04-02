f9a981088757ef388b17250b800118b7c2bde78a
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-02 10:46:15 +0700

    Menampilkan indeks array kedua

---
+++ src/Course/SoloLearn/Arrays/InitializingArrays.java
@@ -0,0 +1,13 @@
+package Course.SoloLearn.Arrays;
+
+public class InitializingArrays {
+
+    public static void main(String[] args) {
+
+        int tmp[ ] = {5, 8, 9, 3};
+
+        System.out.println(tmp[2]);
+
+    }
+
+}
