11a82352b0b36705b753bed82b6670e6ae11aa71
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-03 10:54:03 +0700

    Membuat object dengan kata kunci "new"

---
+++ src/Course/SoloLearn/ClassesAndObjects/CreatingNewObjects.java
@@ -0,0 +1,8 @@
+package Course.SoloLearn.ClassesAndObjects;
+
+public class CreatingNewObjects {
+
+    CreatingNewObjects obj1 = new CreatingNewObjects();
+    CreatingNewObjects obj2 = new CreatingNewObjects();
+
+}
