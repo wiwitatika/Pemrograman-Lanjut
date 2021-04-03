e224648dbe0de0f3261a7c99e1d302e5edeb85dc
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-03 08:29:29 +0700

    Menampilakan teks "hi" dari method hello

---
+++ src/Course/SoloLearn/ClassesAndObjects/Methods.java
@@ -0,0 +1,17 @@
+package Course.SoloLearn.ClassesAndObjects;
+
+public class Methods {
+
+    public static void main(String[ ] args) {
+
+        hello();
+
+    }
+
+    static void hello() {
+
+        System.out.println("hi");
+
+    }
+
+}
