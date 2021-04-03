65cd19b83cd3524bf6ebde4b4a5eb35c11e54d3b
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-03 08:16:46 +0700

    Project

---
+++ src/Course/SoloLearn/BasicConcept/TimeConverterProject.java
@@ -0,0 +1,20 @@
+package Course.SoloLearn.BasicConcept;
+
+import java.util.Scanner;
+public class TimeConverterProject {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int days = scanner.nextInt();
+
+        //your code goes here
+        int hours = days * 24;
+        int minutes = hours * 60;
+        int resultSeconds = minutes * 60;
+
+        System.out.println(resultSeconds);
+
+    }
+
+}
