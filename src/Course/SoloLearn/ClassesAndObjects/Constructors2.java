891e03cf41b9f3ce1964753050ce79172fb06d53
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-03 22:50:13 +0700

    Konstruktor -> metode khusus yang dipanggil ketika sebuah objek dibuat dan digunakan untuk menginisialisasinya

---
+++ src/Course/SoloLearn/ClassesAndObjects/Constructors2.java
@@ -0,0 +1,21 @@
+package Course.SoloLearn.ClassesAndObjects;
+
+public class Constructors2 {
+
+    public static void main(String[] args) {
+
+        class A {
+
+            private int x;
+
+            public A (int val) {
+
+                x = val;
+
+            }
+
+        }
+
+    }
+
+}
