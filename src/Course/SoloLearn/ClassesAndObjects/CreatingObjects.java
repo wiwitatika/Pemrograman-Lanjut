e6f8f6de87106c0861b294657b67dc3190ce776e
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-03 10:22:52 +0700

    Membuat object

---
+++ src/Course/SoloLearn/ClassesAndObjects/CreatingObjects.java
@@ -0,0 +1,22 @@
+package Course.SoloLearn.ClassesAndObjects;
+
+public class CreatingObjects {
+
+    public void test() {
+
+        System.out.println("Hi");
+
+    }
+
+}
+
+class B {
+
+    public static void main(String args[ ]) {
+
+        CreatingObjects obj = new CreatingObjects();
+        obj.test();
+
+    }
+
+}
