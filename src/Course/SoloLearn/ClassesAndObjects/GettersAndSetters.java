b5da0464e50ca32b1e9b95b1f3910cb0cbd9d650
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-03 10:43:18 +0700

    Method get mengembalikan nilainya, sedangkan method set menetapkan nilainya

---
+++ src/Course/SoloLearn/ClassesAndObjects/GettersAndSetters.java
@@ -0,0 +1,24 @@
+package Course.SoloLearn.ClassesAndObjects;
+
+public class GettersAndSetters {
+
+    class A {
+
+        private int x;
+
+        public int getX() {
+
+            return x;
+
+        }
+
+        public void setX(int x) {
+
+            this.x = x;
+
+        }
+
+
+    }
+
+}
