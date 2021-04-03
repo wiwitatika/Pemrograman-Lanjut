81ed470ca75acae1b9d22c4ea3c31e63d61d6b84
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-03 08:44:13 +0700

    Metode yang mengambil beberapa data, yang disebut parameter

---
+++ src/Course/SoloLearn/ClassesAndObjects/MethodParameters.java
@@ -0,0 +1,17 @@
+package Course.SoloLearn.ClassesAndObjects;
+
+public class MethodParameters {
+
+    public static void main(String[ ] args) {
+
+        doSomething(4);
+
+    }
+
+    static void doSomething(int x) {
+
+        System.out.println(x*x);
+
+    }
+
+}
