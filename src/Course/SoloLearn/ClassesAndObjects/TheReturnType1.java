16d7f260c3834d06d631711e89ac5f0a003b5cec
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-03 08:44:13 +0700

    Return (mengembalikan nilai)

---
+++ src/Course/SoloLearn/ClassesAndObjects/TheReturnType1.java
@@ -0,0 +1,19 @@
+package Course.SoloLearn.ClassesAndObjects;
+
+public class TheReturnType1 {
+
+    public static void main(String[ ] args) {
+
+        int x = 10;
+
+        test(x);
+
+    }
+
+    static void test(int x) {
+
+        System.out.println(x);
+
+    }
+
+}
