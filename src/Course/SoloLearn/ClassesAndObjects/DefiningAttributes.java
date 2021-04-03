3ef46ec234706605f10a54d6d9a8efa92b9f6a2d
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-03 10:27:53 +0700

    Mendefinisikan atribut

---
+++ src/Course/SoloLearn/ClassesAndObjects/DefiningAttributes.java
@@ -0,0 +1,13 @@
+package Course.SoloLearn.ClassesAndObjects;
+
+public class DefiningAttributes {
+
+    class Person {
+
+        int age;
+        double height;
+        String name;
+
+    }
+
+}
