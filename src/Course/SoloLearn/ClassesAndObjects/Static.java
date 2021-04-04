3b81606dd6b53fe9e2f9064052075be9e6646557
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-04 15:14:20 +0700

    Ketika mendeklarasikan variabel atau metode sebagai statis, itu milik kelas, bukan milik contoh tertentu

---
+++ src/Course/SoloLearn/ClassesAndObjects/Static.java
@@ -0,0 +1,15 @@
+package Course.SoloLearn.ClassesAndObjects;
+
+public class Static {
+
+    public static int pCount;
+
+    public static void main(String[ ] args) {
+
+        Static.pCount = 1;
+        Static.pCount++;
+        System.out.println(Static.pCount);
+
+    }
+
+}
