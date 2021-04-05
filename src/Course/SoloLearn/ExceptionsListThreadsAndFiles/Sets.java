5a7cea617e9e7d7dcbe79d95d2f2b9f320d95fd5
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-05 14:31:24 +0700

    Sets

---
+++ src/Course/SoloLearn/ExceptionsListThreadsAndFiles/Sets.java
@@ -0,0 +1,13 @@
+package Course.SoloLearn.ExceptionsListThreadsAndFiles;
+
+import java.util.HashSet;
+public class Sets {
+
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<String>();
+        set.add("A");
+        set.add("B");
+        set.add("C");
+        System.out.println(set.size());
+    }
+}
