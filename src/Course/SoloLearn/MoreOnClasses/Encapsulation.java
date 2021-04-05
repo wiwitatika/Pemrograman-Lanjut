dc11dc5404638768eeb24beb9b4a207a3d135aef
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-05 08:19:50 +0700

    Pemrograman berorientasi objek: Enkapsulasi

---
+++ src/Course/SoloLearn/MoreOnClasses/Encapsulation.java
@@ -0,0 +1,17 @@
+package Course.SoloLearn.MoreOnClasses;
+
+public class Encapsulation {
+
+    private int age;
+
+    public void setAge(int age) {
+
+        if (age > 0) {
+
+            this.age = age;
+
+        }
+
+    }
+
+}
