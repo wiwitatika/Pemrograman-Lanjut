b3096e6b693fc3f484028b85debb4db8a183f57f
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-05 13:13:38 +0700

    Exception Handling

---
+++ src/Course/SoloLearn/ExceptionsListThreadsAndFiles/ExceptionHandling.java
@@ -0,0 +1,27 @@
+package Course.SoloLearn.ExceptionsListThreadsAndFiles;
+
+public class ExceptionHandling {
+
+    public static void main(String[] args) {
+
+        int x = 12;
+
+        int y = 0;
+
+        try {
+
+            int z = x / y;
+
+            System.out.println(z);
+
+        }
+
+        catch (Exception e) {
+
+            System.out.println("Error");
+
+        }
+
+    }
+
+}
