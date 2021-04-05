b4235ab79796b9c3fda58c2c24dce1359e771c91
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-05 13:35:47 +0700

    Multiple Exceptions

---
+++ src/Course/SoloLearn/ExceptionsListThreadsAndFiles/Throw.java
@@ -0,0 +1,17 @@
+package Course.SoloLearn.ExceptionsListThreadsAndFiles;
+
+import  java.io.IOException;
+public class Throw {
+
+
+    public  void Do(int x) throws IOException {
+
+        if (x < 0) {
+            throw new IOException();
+        } else {
+
+        }
+
+    }
+
+}
