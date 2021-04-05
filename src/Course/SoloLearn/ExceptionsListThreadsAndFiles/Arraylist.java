cd90b966dafa3ba82720128f5631e370b1166f24
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-05 14:00:15 +0700

    Array List

---
+++ src/Course/SoloLearn/ExceptionsListThreadsAndFiles/Arraylist.java
@@ -0,0 +1,16 @@
+package Course.SoloLearn.ExceptionsListThreadsAndFiles;
+
+import java.util.ArrayList;
+public class Arraylist {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        System.out.println(list.get(1));
+
+    }
+
+}
