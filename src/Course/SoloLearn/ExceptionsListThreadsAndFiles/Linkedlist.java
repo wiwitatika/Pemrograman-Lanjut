1b8c5008f78d8eb3deb87d554029c56b128cb3a2
wiwitatika <12050123513@students.uin-suska.ac.id>
2021-04-05 14:12:44 +0700

    Linked List

---
+++ src/Course/SoloLearn/ExceptionsListThreadsAndFiles/Linkedlist.java
@@ -0,0 +1,12 @@
+package Course.SoloLearn.ExceptionsListThreadsAndFiles;
+
+import java.util.LinkedList;
+public class Linkedlist {
+
+    public static void main(String[] args) {
+        LinkedList <String> list = new LinkedList<String>();
+        list.add("hallo");
+        System.out.println(list.get(0));
+    }
+
+}
